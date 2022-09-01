9f17129532f8bb6e127198ff4344fa57c929743e
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-09-01 09:56:05 -0400

    terminado o totolotoBookVersion, faltando apenas compilar e executar

--- livro-liguagemC/capitulo6/totoloto/totolotoBookVersion.c
+++ livro-liguagemC/capitulo6/totoloto/totolotoBookVersion.c
@@ -11,5 +11,99 @@ void Iniciador_Random();
 void Iniciador_Vetor(int v[NUMEROS]);
 int Ler_Numeros_Apostar();
 void Gerar(int *v, int n); //preciso testar
-void Apresentar(int resultado[]); 
+void Apresentar(int resultado[]);
 
+/*
+    Inicia o gerador de numeros aleatorios com um numero que depende da hora que se
+    executa a função
+*/
+
+void Iniciador _Random()
+{
+    long ultime;
+    time(&ultime);
+    srand((unsigned) ultime); //fazendo cast
+}
+
+/* inicia com zero o vetor de controle dos numeros selecionados*/
+void Iniciador(int v[])
+{
+    for(int i = 0; i < NUMEROS; i++)
+        v[i] = -;
+}
+
+/*
+    Lê o numeros da quantidade a apostar. Aceita valores entre MIN_APOSTA e NUMEROS.
+    Aceita o valor de zero para indicar o termino do programa
+*/
+
+int Ler_Numeros_Apostar()
+{
+    int n;
+
+    do
+    {
+        printf("Quantos números quer apostar (0 - terminar): ");
+        scanf("%d", &n);
+    } while ((n < MIN_APOSTA || N > NUMEROS) && n != 0);
+
+    return n;   
+}
+
+/*
+    Gera "n" numeros aleatorios e coloca a respectiva posição do vetor com o 
+    valor 1 que indica que esse numeros foi selecionado
+*/
+
+void Gerar(int *v, int n)
+{
+    int i, indice;
+    for(i = 1; i < n; i++)
+    {
+        indice = rand() % NUMEROS; //Devolve números aleatorios entre 0 e NUMEROS - 1, é sorteado um números pelo indice mesmo!!
+        if(v[indice] == 0)
+            v[indice] = 1;
+        else
+            i--;
+    }
+}
+
+
+/*
+    apresenta uma grade de numeros e o selecionados irão aparecer em formato de  XX.
+*/
+
+void Apresentar(int resultado[])
+{
+    for(int i = 0; i < NUMEROS; i++)
+    {
+        if(resultado[i] == 0)
+            printf(" %2d", i + 1);
+        else
+            printf(" XX");
+        
+        if((i + 1) % NUMERO_LINHA == 0)
+            putchar('\n');
+    }
+
+    putchar('\n');
+}
+
+int main(void)
+{
+    int vetor[NUMEROS]
+    int quantidadeNumeros = 0; //quantidade de numeros para jogar no jogo - 6..49
+
+    Iniciador_Random();
+    while(1)
+    {
+        Iniciador(vetor); //iniciando o array com zero
+
+        //o retorno de ler_Numeros_Apostar 
+        if((quantidadeNumeros = Ler_Numeros_Apostar()) == 0)
+            break;
+        
+        Gerar(vetor, quantidadeNumeros); //Gera os numeros a apostar, não tem retorno
+        Apresentar(vetor);
+    }
+}
