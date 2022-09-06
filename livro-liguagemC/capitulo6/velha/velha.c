c0e7492aea0d77135bd6ae86ca28bff6ac23bd2f
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-09-05 22:29:51 -0400

    adicionado os comentarios sobre estrategias para fazer o codigo da velha

--- livro-liguagemC/capitulo6/velha/velha.c
+++ livro-liguagemC/capitulo6/velha/velha.c
@@ -1,3 +1,19 @@
 /*
    faça um jogo da velha jogavel, jogo da velha manual 
+
+   entrada: 
+   verificar se o caractere  "x" ou "O"
+      - bastar utilizar o tabela ascii para os tais caracteres
+      - pode utilizar a lib da cs50 para entreda de caracteres
+   
+   imprimir uma grade com os com a identificação de letras e numeros. (igual campo minado)
+      -criar uma estrategia para colocar identificação na colunas e linhas
+
+   para completar o jogo, vencer ou dá "velha", e necessario que o array em tres posições em sequencia
+   estejam preenchidas pelo mesmo caracter!
 */
+
+
+
+
+
