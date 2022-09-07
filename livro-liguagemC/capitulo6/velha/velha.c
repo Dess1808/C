c24cb6d4c0132a155018b4593f81b87a7d016c0e
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-09-07 02:21:17 -0400

    foi adicionado comentarios de rascunho no arquivo .c principal, no caso, velha.c

--- livro-liguagemC/capitulo6/velha/velha.c
+++ livro-liguagemC/capitulo6/velha/velha.c
@@ -2,16 +2,22 @@
    faça um jogo da velha jogavel, jogo da velha manual 
 
    entrada: 
-   verificar se o caractere  "x" ou "O"
+   [x]verificar se o caractere  "x" ou "O"
       - bastar utilizar o tabela ascii para os tais caracteres
       - pode utilizar a lib da cs50 para entreda de caracteres
    
-   imprimir uma grade com os com a identificação de letras e numeros. (igual campo minado)
+   [x]imprimir uma grade com os com a identificação de letras e numeros. (igual campo minado)
       -criar uma estrategia para colocar identificação na colunas e linhas
 
    para completar o jogo, vencer ou dá "velha", e necessario que o array em tres posições em sequencia
    estejam preenchidas pelo mesmo caracter!
+
+   rascunhos:
+   X - 89 decimal
+   O - 79 decimal
 */
+
+
 
 
 
