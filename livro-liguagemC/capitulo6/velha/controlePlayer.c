89f519595ddcc5d7d5254331642a96086ec80c34
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-09-13 22:42:54 -0400

    controle de jogador concluido, esta sem restição de colunas e linhas

--- livro-liguagemC/capitulo6/velha/controlePlayer.c
+++ livro-liguagemC/capitulo6/velha/controlePlayer.c
@@ -17,7 +17,7 @@ int main(void)
     
     if(toupper(player1Valor) == 'X')
     {
-        printf("%s irá ficar com \'O\'\n", player2);
+        printf("%s irá ficar com \'O\'\n", player2); 
         player2Valor = 'O';
     }
     else
@@ -26,6 +26,65 @@ int main(void)
         player2Valor = 'X';
     }
 
-    printf("%s - %c\n", player1, player1Valor);    
-    printf("%s - %c\n", player2, player2Valor);    
+    //utilizar o valor do jogador 1 como parametro - 'X' ou 'O'
+    char jogadaMomento = player1Valor;
+    int count = 0;
+    int coluna = 0;
+    int linha = 0;
+    char jogada = 0;
+    char vetorFakeNaPosicaoTal;
+    string player = player1;
+    bool estadoSalvo = true;
+
+    //preciso continuar
+    //criar um switch de controle
+    while(1)
+    {
+        //codigo de entrada de estado atual
+        //sempre começar com o play1 ou "X"
+        //linha, coluna e jogada
+        //por enquanto sem restrições
+        printf("vez do jogador %s\n", player);
+        linha = toupper(get_int("linha: "));
+        coluna = toupper(get_int("coluna: "));
+        jogada = toupper(get_char("jogada: "));
+
+
+        //verificador de entrada correta
+        if(jogada == jogadaMomento)
+        {
+            //deve ser feito a atribuição no vetor
+            vetorFakeNaPosicaoTal = jogada;
+
+            //troca de variavel "jogadaEstado" utilizando bool
+            if(estadoSalvo)
+            {
+                jogadaMomento = player2Valor;
+                player = player2;
+                estadoSalvo = false;
+            }
+            else
+            {
+                jogadaMomento = player1Valor;
+                player = player1;
+                estadoSalvo = true;
+            }
+
+            count++;
+        }
+        else
+        {
+            //informar que o valor inserido não confere com a requição atual
+            printf("valor inserido esta errado\n");
+        }
+
+        //contador
+        if(count >= 9)
+        {
+            printf("Deu Velha!\n");
+            break;
+        }
+
+        printf("contador de jogadas: %d\n\n", count);
+    }    
 }
