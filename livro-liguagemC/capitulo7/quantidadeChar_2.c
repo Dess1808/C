9afcce58f78a07d44b172ee5bc2c76fbd485cf04
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-10-04 20:56:01 -0400

    versão com for utilizando o caracter delimitador nulo como condição

---
+++ livro-liguagemC/capitulo7/quantidadeChar_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int countChar(char *v);
+
+int main(void)
+{
+    char vetor[0];
+
+    scanf("%s", vetor);
+    printf("%i\n", countChar(vetor));
+    printf("%s\n", vetor); 
+}
+
+int countChar(char *v)
+{
+    int i = 0;
+
+    for(i = 0; v[i]; i++);//nulo e false
+
+    return i;
+}
