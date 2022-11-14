9b4756a950a873654961a8fdb79db96e0dcfad30
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-11-14 13:53:53 -0400

    codigo resetado

--- beecrowd/greatest/greatest.c
+++ beecrowd/greatest/greatest.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int Greatest(int a, int b, int c);
-int ValorAbs(int n);
-int MaiorAB(int a, int b);
 
 int main(void)
 {
@@ -13,24 +10,5 @@ int main(void)
     printf("b: "); scanf("%d", &b);
     printf("c: "); scanf("%d", &c);
 
-    printf("%d eh o maior\n", Greatest(a, b, c));
-}
-
-int ValorAbs(int n)
-{
-    if(n < 0)
-        return n *= -1;
-    else 
-        return n;
-}
-
-//verificar se esta retornando certo
-int MaiorAB(int a, int b)
-{
-    return (a + b + ValorAbs(a - b)) / 2;
-}
-
-int Greatest(int a, int b, int c)
-{
-    //return (MaiorAB(MaiorAB(a, b), c) + ValorAbs(ValorAbs(a - b) - c)) / 2;
+    printf("%d eh o maior\n");
 }
