408bba5b5ba29ea01786e9acad45e4d56ca618e1
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-11-18 13:53:31 -0400

    adicionando como exemplo so livro, testado e funcionando

---
+++ livro-liguagemC/capitulo7/strcountc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<cs50.h>
+
+char strcountc(char *exemplo, char ch);
+
+int main(void)
+{
+    string exemplo = get_string("string: ");
+    char letra = get_char("letra: ");
+
+    printf("%s (%c) - %i\n", exemplo, letra, strcountc(exemplo, letra));
+
+}
+
+char strcountc(char *exemplo, char ch)
+{
+    int i, count = 0;
+
+    for(i = 0; exemplo[i] != '\0'; i++)
+    {
+        if(exemplo[i] == ch)
+            count++;
+    }
+
+    return count;
+}
