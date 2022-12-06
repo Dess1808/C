21f0542a0293efcedaa1fb9c1c3cbd2557645069
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-12-06 13:27:29 -0400

    continuar apartir da nota 20

--- beecrowd/banknotesAndCoins/backNotesAndCoins.c
+++ beecrowd/banknotesAndCoins/backNotesAndCoins.c
@@ -1,6 +1,57 @@
 #include<stdio.h>
 
+//constantes notas e moedas
+//notas
+const int CEM = 100;
+const int CINQUENTA = 50;
+const int VINTE = 20;
+const int DEZ = 10;
+const int CINCO = 5;
+const int DOIS = 2;
+
+//coins
+const float COIN1 = 1.00;
+const float COIN050 = 0.50;
+const float COIN025 = 0.25;
+const float COIN010 = 0.10;
+const float COIN005 = 0.05;
+const float COIN001 = 0.01;
+
+
 int main(void)
 {
+    float valor = 0.0;
+
+    //recebendo valores
+    do
+    {
+        scanf("%f", &valor);
+    } while (valor <= 0 || valor >= 1000000.00);
+    
+    //contadores
+    int countCem, countCinquenta, countVinte, countDez, countCinco, countDois, count1, count050, count025, count010, count005, count001 = 0;
+
+    //notas
+    //100
+    while(valor >= CEM)
+    {
+        countCem++;
+        valor -= CEM;
+    }
+
+    //50
+    while(valor >= CINQUENTA)
+    {
+        countCem++;
+        valor -= CINQUENTA;
+    }
+
+    //20
+    while(valor >= CEM)
+    {
+        countCem++;
+        valor -= CEM;
+    }
+
     
 }
