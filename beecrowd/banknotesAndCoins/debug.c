a55fdebf9a51712938201a68c6e921b92fc18de6
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-12-12 10:41:45 -0400

    zerado a linhas de codigo

--- beecrowd/banknotesAndCoins/debug.c
+++ beecrowd/banknotesAndCoins/debug.c
@@ -6,24 +6,5 @@
 
 int main(void)
 {
-    float valor = get_float("valor a ser contado: ");
-
-    int count1 = 0;
-    int count050 = 0;
-
-    while(valor >= 1)
-    {
-        count1++;
-        valor -= 1;
-    }
-
-    while(valor >= 0.50)
-    {
-        count050++;
-        valor -= 0.50;
-    }
-
-    printf("%d moeda(s) de R$ 1.00\n", count1);
-    printf("%d moeda(s) de R$ 0.50\n", count050);
 
 }
