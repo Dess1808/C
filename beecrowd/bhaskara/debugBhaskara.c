d0d5fd02696e5251833e3951b65da1f7c372d20d
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-12-15 13:27:27 -0400

    inciado, faltando fazer as restricoes do delta

---
+++ beecrowd/bhaskara/debugBhaskara.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h> //nao reconhece!!
+
+int main(void)
+{
+    double a = get_double("A: ");
+    double b = get_double("B: ");
+    double c = get_double("C: ");
+
+    //delta
+    double delta = pow(b, 2) - 4 * a * c;
+
+    //bhaskara
+    double x1 = ((-b) + (sqrt(delta))) / (2 * a);
+    double x2 = ((-b) - (sqrt(delta))) / (2 * a);
+
+    printf("x1: %f\n", x1);
+    printf("x2: %f\n", x2);
+}
