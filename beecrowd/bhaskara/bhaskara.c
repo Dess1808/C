63a3aa097498a4d68c82b9c30ad7d80ca8e0535d
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-12-16 09:43:28 -0400

    finalizado e funcionando

--- beecrowd/bhaskara/bhaskara.c
+++ beecrowd/bhaskara/bhaskara.c
@@ -10,9 +10,22 @@ int main(void)
     scanf("%lf %lf %lf", &a, &b, &c);
 
     //delta
-    double delta = pow(b, 2) - 4 * a * c;   
+    double delta = pow(b, 2) - 4 * a * c; //esse cara
 
-    //verificar impossibilidade de cauculos de raizes
-    
+    //teste
+    if((a == 0) || (delta < 0))
+    {
+        printf("Impossivel calcular\n");
+    }
+    else
+    {
+        //bhaskara
+        double x1 = ((-b) + (sqrt(delta))) / (2 * a);
+        double x2 = ((-b) - (sqrt(delta))) / (2 * a);
+
+        printf("R1 = %.5f\n", x1);
+        printf("R2 = %.5f\n", x2);
+    }
+
     return 0;
 }
