06eacd44a71978c0fa7536a539f4720f437b0cde
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-12-15 22:48:19 -0400

    incompleto ainda, verificar a impossibilidade de razies negativas e zero

---
+++ beecrowd/bhaskara/bhaskara.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+    double a;
+    double b;
+    double c;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    //delta
+    double delta = pow(b, 2) - 4 * a * c;   
+
+    //verificar impossibilidade de cauculos de raizes
+    
+    return 0;
+}
