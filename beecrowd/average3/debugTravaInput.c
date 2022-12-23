d5d501065a269e083a76860d270c6a489d77d99c
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-12-23 11:41:18 -0400

    debug teste de entradas, continuar verificando

--- beecrowd/average3/debugTravaInput.c
+++ beecrowd/average3/debugTravaInput.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 #include<cs50.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main(void)
 {
     //e se a entrada for em string
+
+    //limitar o que sera utilizado para os calculos, 3 digitos no caso
     char *n;
+
+    scanf("%s",n);
     
-    do
+    for(int i = 0; i < strlen(n); i++)
     {
-        scanf("%s", n);
-        printf("%.1f\n", atof(n));
-    } while (atof(n) == 0.0);
+        printf("%c ", n[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
