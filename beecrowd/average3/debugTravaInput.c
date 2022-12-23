fa1ad603bdc68104a9cba0f4bbc606d203e9d4ce
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-12-23 10:39:42 -0400

    teste se a entreda for string convertendo para float

--- beecrowd/average3/debugTravaInput.c
+++ beecrowd/average3/debugTravaInput.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
-
-//travar entradas somente em numeros
+#include<cs50.h>
+#include<stdlib.h>
 
 int main(void)
 {
+    //e se a entrada for em string
+    char *n;
     
+    do
+    {
+        scanf("%s", n);
+        printf("%.1f\n", atof(n));
+    } while (atof(n) == 0.0);
+
+    return 0;
 }
+
