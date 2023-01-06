53b75e92e3e36c625c10977c2e71456fd7d08e40
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-06 13:43:12 -0400

    problema ja abstraido em comentarios, so olhar!

---
+++ beecrowd/triangle/triangleDebug.c
@@ -0,0 +1,14 @@
+/*
+    condicao para a existencia de um triangulo
+    https://pt.wikipedia.org/wiki/Tri%C3%A2ngulo
+
+    area calculo trapezio
+    https://brasilescola.uol.com.br/matematica/quadrilateros-e-trapezio.htm
+
+    obs codigo:
+    verificar se o 3 valores inseridos de ponto flutuantes dao condicao a um triagulo
+    se sim
+        calcular o perimetro (p = a + b + c)
+    senao
+        calcucar a area como um trapezio
+*/
