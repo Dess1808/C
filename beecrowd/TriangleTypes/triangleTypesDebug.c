860242febf18c4c8b20664830e602a0873c5e237
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-09 13:23:33 -0400

    nocoes e logica sendo construidas, continuar processo

---
+++ beecrowd/TriangleTypes/triangleTypesDebug.c
@@ -0,0 +1,15 @@
+/*
+    definir os tipos de triagulos e triagulos enquanto a agulos
+
+    Equilatero - todos os lados iguais
+    Isoceles - pelo menos dois iguais
+    Scaleno - todos diferentes
+
+    triagulos angulos 
+    Retandulo - um agulo e de 90 graus
+    Acutangulo - todos os agulos sao agudos, ou seja, todos os agulos sao menores que 90
+    Obtusngulo - pelo um angulo e maior que 90
+
+    logica
+
+*/
