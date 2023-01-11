341bd31b632e546eb546733681f97260005cdb71
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-11 13:54:00 -0400

    iniciado a versao final, apenas olhar os comentarios e continuar

---
+++ beecrowd/TriangleTypes/triangleTypes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+#define SIZE 3
+
+int main(void)
+{
+    double abc[SIZE]
+
+    //por enquanto sem tramentento de entrada
+    scanf("%1.lf%1.lf%1.lf", abc[0], abc[1], abc[2]);
+
+    //ordenando
+
+    //atribuir a A B C ja ordenado
+
+    //classificar o possivel triangulo
+}
