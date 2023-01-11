ad89b5df685c386567be167657bf7f70c235fe21
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-11 13:18:00 -0400

    versao de teste e debug de possibilidade

---
+++ beecrowd/TriangleTypes/debug.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+#define SIZE 3
+
+int main(void)
+{
+    // char *r = "valor inicial";
+    // r = "outro valor";
+    // printf("%s\n", r);
+    // return 0;
+
+    //ordem decrecente
+    // double abc[SIZE];
+    // scanf("%lf%lf%lf", &abc[0], &abc[1], &abc[2]);
+
+    // int newSize = SIZE - 1;
+    // int maiorAtual = 0;
+    // int trocar = 0;
+    // int ultimaPosicaoMaior = 0;
+    // int auxiliar = 0;
+
+    // for(int i = 0; i < newSize; i++)
+    // {
+    //     maiorAtual = abc[i];
+
+    //     for(int j = i + 1; j <= newSize; j++)
+    //     {
+    //         //so executado de for maior
+    //         //verificar logica crecente (>) e decrecente (<)
+    //         if(maiorAtual < abc[j])
+    //         {
+    //             maiorAtual = abc[j];
+    //             ultimaPosicaoMaior = j;
+    //             trocar = 1;
+    //         }
+    //     }
+
+    //     //verificando troca 
+    //     if(trocar)
+    //     {
+    //         auxiliar = abc[i];
+    //         abc[i] = abc[ultimaPosicaoMaior];
+    //         abc[ultimaPosicaoMaior] = auxiliar;
+
+    //         //controle 
+    //         trocar = 0;
+    //     }
+    // }
+
+
+    // for(int i = 0; i < SIZE; i++)
+    // {
+    //     printf("%1.lf ", abc[i]);
+    // }
+
+    // putchar('\n');
+
+}
