1847284c8c1350f4577196e73eb9870a0666cd9d
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-16 09:41:35 -0400

    versao 1.2 finalizada, porem nao funcionou, continuar com bug, continuar verificando

--- beecrowd/gameTimeWithMinutes/gameTimeWithMinutesDebug.c
+++ beecrowd/gameTimeWithMinutes/gameTimeWithMinutesDebug.c
@@ -1,49 +1,43 @@
 /*
-    logica versao 1:
+    logica versao 1: - NAO FUNCIONA
     transformar hora em minutos (x60) eh somar com a entrada de "minuto", das duas entradas de hora, minuto
     tirar a diferenca em os dois
     duracaoMinutos sera o valor absoluto da diferenca
     duracaoHora sera o valor absoluto da diferenca / 60. obs: apenas a parte inteiro
 
-    logica versao 1.2:
-*/
+    logica versao 1.2: - NAO FUNCIONA
+    se ((horaStart == horaEnd) && (minutoStart > minutoEnd))
+        devo calcular somento "minutos", nao chegou a bater 1h
+    senao 
+        calcular diferenca de horaStart e horaEnd
+        calcular diferenca de minutoStart e minutoEnd (obs: se resultado for == a 60, o valor final deve ser 0)
 
-//verificar, logica ainda nao bate!!!
+    logica versao 1.3: 
+
+*/
 
 #include<stdio.h>
 #include<cs50.h>
 
+#define HOUR 24
 #define MINUTE 60
-
-int Absolute(int x);
 
 int main(void)
 {
-    //input and variable inicialization
+    //variable and inicialization
     int hourStart = get_int("hora inicial: ");
-    int minuteStart = get_int("minuto incial: ");
-    int hourEnd = get_int("hora final: ");
+    int hourEnd = get_int("minuto inicial: ");
+    int minuteStart = get_int("minuto final: ");
     int minuteEnd = get_int("minuto final: ");
 
-    //total de horas em minutos
-    int totalHourMinutesStart = (hourStart * MINUTE) + minuteStart;
-    int totalHourMinutesEnd = (hourEnd * MINUTE) + minuteEnd; 
+    int durationHour = 0;
+    int durationMinutes = 0;
 
-    //obtendo absolute da diferenca, jah como resultado
-    int durationHour = Absolute(totalHourMinutesStart - totalHourMinutesEnd) / 60;
-    int durationMinute = Absolute(totalHourMinutesStart - totalHourMinutesEnd);
+    //control flux
 
-    //output
-    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", durationHour, durationMinute);
+    //ouput
+    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", durationHour, durationMinutes);
 
     return 0;
 }
 
-
-int Absolute(int x)
-{
-    if(x < 0)
-        return x *= -1;
-    else
-        return x;
-}
