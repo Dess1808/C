a0e821ef08768e32ecb6d19e3e6d3ab359aa7628
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-16 11:02:42 -0400

    versao debug finalizada, enfim, esta funcionando!!

--- beecrowd/gameTimeWithMinutes/gameTimeWithMinutesDebug.c
+++ beecrowd/gameTimeWithMinutes/gameTimeWithMinutesDebug.c
@@ -13,27 +13,59 @@
         calcular diferenca de minutoStart e minutoEnd (obs: se resultado for == a 60, o valor final deve ser 0)
 
     logica versao 1.3: 
+    transformar hora em minutos, somar + minutos e calcular diferenca
+    calcular diferenca com 1440 (24h)
+    o resultado sera o minutos obs(se > 60, entao, dividir resultado por 60 e o resultado sera o valor em minuto)
+    dividir por 60 para obter o valor em hora
+    
+    Obs: um pouco estranho mais pode funcionar
 
 */
 
 #include<stdio.h>
 #include<cs50.h>
 
-#define HOUR 24
 #define MINUTE 60
+#define HORAMINUTO 1440
 
 int main(void)
 {
     //variable and inicialization
     int hourStart = get_int("hora inicial: ");
-    int hourEnd = get_int("minuto inicial: ");
-    int minuteStart = get_int("minuto final: ");
+    int minuteStart = get_int("minuto inicial: ");
+    int hourEnd = get_int("hora final: ");
     int minuteEnd = get_int("minuto final: ");
 
+    //variables control
     int durationHour = 0;
     int durationMinutes = 0;
+    int resultadoHoraMinuto = 0;
+    int diferencaHoraMinuto = 0;
+    int diferencaMinute = 0;
 
-    //control flux
+    //diferenca
+    diferencaHoraMinuto = ((hourStart * 60) + minuteStart) - ((hourEnd * 60) + minuteEnd);
+
+    //tratando a diferenca por 1440
+    if (diferencaHoraMinuto < 0)
+        resultadoHoraMinuto = (HORAMINUTO - diferencaHoraMinuto) - HORAMINUTO;
+    else 
+        resultadoHoraMinuto = HORAMINUTO - diferencaHoraMinuto;
+
+    //obtendo hora convertendo em hora
+    durationHour = resultadoHoraMinuto / MINUTE;
+
+    //obtendo minuto
+    diferencaMinute = minuteStart - minuteEnd;
+
+    if (diferencaMinute < 0)
+        durationMinutes = (MINUTE - diferencaMinute) - MINUTE;
+    else 
+        durationMinutes = MINUTE - diferencaMinute;
+
+    //ja contou 1h
+    if (durationMinutes == MINUTE)
+        durationMinutes = 0;
 
     //ouput
     printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", durationHour, durationMinutes);
