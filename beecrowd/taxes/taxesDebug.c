98fb08cbdb5caf775f8872cd7b33b909f11b2b7f
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-24 13:56:36 -0400

    levantando hipoteses

---
+++ beecrowd/taxes/taxesDebug.c
@@ -0,0 +1,25 @@
+/*
+    hipotose 1:
+    tirar as diferencas e montar um tabelas de resultados ja com a porcentagem
+
+    link base
+    https://plantaocontabil.com.br/blog/parcelas-a-deduzir-do-imposto-de-renda/
+
+    
+
+    0.00 - 2000.00 - inseto
+    2000.01 - 3000.00 (8%)  80.00
+    3000.01 - 4500.00 (18%) 419.20 3002.00 
+    acima de 4500.00 (28%) 1260.00
+
+    status: NAO FUNCIONOU
+    
+    Hipotese 2:
+    link base
+    https://ingracio.adv.br/contribuicoes-inss/
+
+    somar valores de aliquota ateriores
+
+
+
+*/
