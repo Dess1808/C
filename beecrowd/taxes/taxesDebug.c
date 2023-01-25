040b0c81dce9cbbd2dd6c633a5dfca01b3739511
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-25 11:22:52 -0400

    terceira hipotese em levantamento

--- beecrowd/taxes/taxesDebug.c
+++ beecrowd/taxes/taxesDebug.c
@@ -5,11 +5,9 @@
     link base
     https://plantaocontabil.com.br/blog/parcelas-a-deduzir-do-imposto-de-renda/
 
-    
-
     0.00 - 2000.00 - inseto
     2000.01 - 3000.00 (8%)  80.00
-    3000.01 - 4500.00 (18%) 419.20 3002.00 
+    3000.01 - 4500.00 (18%) 419.20 
     acima de 4500.00 (28%) 1260.00
 
     status: NAO FUNCIONOU
@@ -18,8 +16,18 @@
     link base
     https://ingracio.adv.br/contribuicoes-inss/
 
-    somar valores de aliquota ateriores
+    status: NAO FUNCINOU
 
-
-
+    HIPOTESE 3:
+    ainda procurando....
 */
+
+#include<stdio.h>
+
+int main(void)
+{
+    double salary = get_double("salary: ");
+
+    //definido as faixas
+    if()
+}
