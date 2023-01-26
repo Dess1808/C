87aeee79d4fe48b4a68b247ca945cd6c656a4928
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-26 13:13:55 -0400

    codigo finalizado, testado e funcionando, versao de alternativa finalizada

--- beecrowd/taxes/taxesDebug.c
+++ beecrowd/taxes/taxesDebug.c
@@ -6,7 +6,7 @@
     https://plantaocontabil.com.br/blog/parcelas-a-deduzir-do-imposto-de-renda/
 
     0.00 - 2000.00 - inseto
-    2000.01 - 3000.00 (8%) 1000.00 - 0.08 
+    2000.01 - 3000.00 (8%) 1000.00 - EIGHTP 
     3000.01 - 4500.00 (18%) 1500.00 - 0.18
     acima de 4500.00 (28%) 4500.00 - 0.28
 
@@ -28,15 +28,58 @@
 
     HIPOTESE 4:
 
-    utilizar valores de porcentagem fixa, e somar esses valores tirando a diferenca com o salario inserio
+    logica descrita:
+    utilizar valores de porcentagem fixa, e somar esses valores tirando a diferenca com o salario inserio, 
+    depois tirar a porcentagem da diferenca de acordo com a trate do salario.
 */
 
 #include<stdio.h>
+#include<cs50.h>
+
+#define EIGHTP 0.08
+#define EIGHTEENP 0.18
+#define TWENTYEIGHTP 0.28
+
+void Show(double r);
 
 int main(void)
 {
     double salary = get_double("salary: ");
+    double resultado;
 
-    //definido as faixas
-    if()
+    //controle variaveis 
+    int EightRateDiference = 1000 * EIGHTP;
+    int EightTeenRateDiference = 1500 * EIGHTEENP;
+
+    //logica
+    if (salary <= 2000.00)
+    {
+        //inseto
+        printf("Isento\n");
+    }
+    else if ((salary >= 2000.01) && (salary <= 3000.00))
+    {
+        //aplicar %8 do salario
+        resultado = (salary - 2000) * EIGHTP;
+        Show(resultado);
+    }
+    else if ((salary >= 3000.01) && (salary <= 4500.00))
+    {
+        //aplicar %18 a resto do salario
+        resultado = EightRateDiference + ((salary - 3000) * EIGHTEENP);
+        Show(resultado);
+    }
+    else if (salary > 4500)
+    {
+        //aplica %28 ao resto do salario
+        resultado = (EightRateDiference + EightTeenRateDiference) + ((salary - 4500) * TWENTYEIGHTP);
+        Show(resultado);
+    }
+
+    return 0;
+}
+
+void Show(double r)
+{
+    printf("R$ %.2lf\n", r);
 }
