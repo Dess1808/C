a328c1f35a202c2f364e83d7cb2c196dcd14db0c
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-01-31 13:07:14 -0400

    finalizado versao alternativa, funcionando, problema resolvido em minutos

--- beecrowd/EvenTime/evenTimeDebug.c
+++ beecrowd/EvenTime/evenTimeDebug.c
@@ -1,93 +1,15 @@
-/*
-    logica principal
-    tirar a diferenca entre dias e horas
-
-    rascunho
-    24h em segundo 86400s
-    60m em segundos 3600s
-
-    8:12:23
-    tudo em segundos
-
-    8 - 28.800
-    12 - 720
-    23 - 23
-
-    743
-    29513
-
-    06:13:23
-
-    6 - 21600
-    13 - 780
-    23 - 23
-
-    803
-    22430
-
-    7083
-
-    hora 86400s - 7083s / 3600
-
-    minuto 60 -(-60) / 60
-
-    segundos 23 - 23
-
-    obs: tratamento especial para quando for 24h completas, 1 dia, horas zerdas
-
-    logica principal:
-    transformar hora e minuto em segundos das entredas (transformar na menor unidade que eh "segundos") da duas entradas de horas
-    tirar a diferenca entre inicio e fim e sair transformando de volta para hora minuto e segundo
-
-    obs: todas a unidade devem ser calculadas em "segundos" para depois serem devidamente transformadas para suas unidades finais
-
-    formula principal horas:
-    tirar a diferenca 
-        diferencaHora (horaSegundoStart + horaSegundoMinuto + segundosStart) - (horaSegundoEnd + minutoSegundoEnd + segundoEnd)
-        diferencaMinuto (horaSegundoMinuto + segundosEnd) - (minutoSegundoEnd + segundoEnd)
-        diferencaSegundos (segundo) - (segundo)
-        
-        calculando hora 
-            if(diferencaX < 0)
-                xFinal = (HORASEGUNDO - deferencaX) - HORASEGUNDO / valorRelogio;
-            else 
-                xFinal = HORASEGUNDO - diferencaX / valorRelogio;
-    
-        calculando minuto
-            mesmo calculo
-        calculando segundo
-            mesmo calculo 
-        
-    Obs: 
-    se hora 24, minuto 60 e segundo 60
-    significa que temos um dia completo
-        zerar hora, minuto e segundo, e somar a diferenca absoluta de "dias"
-
-
-
-    obs: o evento e de apenas 1 mes???
-    calcular a quantidade de dias entre os dois dias informados
-    valor absoluto entre os dias -1
-
-    OBS: se a diferenca de dias for 0 e a diferenca de horas der 24h, significa que bateu um dia,
-    devo zerar as horas e informar apenas os dias
-    formula para dia:
-    (diaStart - diaEnd) - 1 vai ser a diferenca entre total de dias
-
-    obervacoes: 
-*/
-
 #include<stdio.h>
 
 #define HOURINSECOND 3600
 #define MINUTESECOND 60
 #define SECOND 60
+
 #define TWENTYFOURINSECOND 86400
+#define SIXTYINSECOND 3600
 
 int Abs(int x);
 
-int main(void)
-{  
+int main(void) {  
     //variable control
     int dayStart = 0;
     int hourStart = 0;
@@ -118,20 +40,17 @@ int main(void)
     int minuteDifference = ((minuteStart * MINUTESECOND) + secondStart) - ((minuteEnd * MINUTESECOND) + secondEnd);
     int secondDifference = secondStart - secondEnd;
 
-    /*
-        CALCULANDO E TRANSFORMANDO (HORA, MINUTO E SEGUNDO)
-    */
     //hour
     if(hourDifference < 0)
         hourFinal = ((TWENTYFOURINSECOND - hourDifference) - TWENTYFOURINSECOND) / 3600;
     else 
-        hourFinal = (TWENTYFOURINSECOND - hourDifference) / 3600;        
+        hourFinal = (TWENTYFOURINSECOND - hourDifference) / 3600;       
 
     //minute
     if(minuteDifference < 0)
-        minutefinal = ((MINUTESECOND - minuteDifference) - MINUTESECOND) / 60;
+        minutefinal = ((SIXTYINSECOND - minuteDifference) - SIXTYINSECOND) / 60;
     else 
-        minutefinal = (MINUTESECOND - minuteDifference) / 60;
+        minutefinal = (SIXTYINSECOND - minuteDifference) / 60;
     
     //second final
     if(secondDifference < 0)
@@ -139,25 +58,34 @@ int main(void)
     else 
         secondFinal = SECOND - secondDifference; 
 
-    /*
-        VERIFICAR SITUAÇÃO DE HORA, MINUTOS E SEGUNDOS
-    */
-
+    //hora verification
     if (hourFinal == 24)
         hourFinal = 0;
     
+    /*
+    caso especial, pois se o valor de minuto for , como ja estamos dividindo por 60, o resultado
+    */
     if (minutefinal == 60)
         minutefinal = 0;
 
+    //pos quando divide por 60 o resultado e 1
     if (secondFinal == 60)
         secondFinal = 0;
 
-
-    //calc day final 
+    //calc day final, deixar negativo para o codigo final
     int dayFinal = Abs(dayStart - dayEnd);
 
-    /*feito as diferencas com calculo final, partir para a verificao de valore*/
-    printf("%d\n%d\n%d\n", hourFinal, minutefinal, secondFinal);
+    //resolvendo diferenca de dias
+    if (dayFinal != 0) 
+        dayFinal--;
+
+    //add + um dia se em horas der 24h
+    if ((hourFinal == 0) && (minutefinal == 0) && (secondFinal == 0))
+        dayFinal++;
+
+    
+    printf("%d dia(s)\n", dayFinal);
+    printf("%d Hora(s)\n%d minuto(s)\n%d segundo(s)\n", hourFinal, minutefinal, secondFinal);
 
     return 0; 
 }
