b342709ad4a32b9fd611d977bb657e5ce8047d7b
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-02-10 09:39:42 -0400

    codigo finalizado, testado e funcionando

--- beecrowd/Interval2/interval2.c
+++ beecrowd/Interval2/interval2.c
@@ -17,39 +17,36 @@ int main(void)
     scanf("%d", &n);
 
     //main flux
-    if ((n > 0) && (n < 1000))
+    //array of x
+    size = n; int numbers[n];
+
+    //input 2 - loop
+    for (int i = 0; i < size; i++)
     {
-        //array of x
-        size = n; int numbers[n];
-
-        //input 2 - loop
-        for (int i = 0; i < size; i++)
-        {
-            scanf("%d", &numbers[i]);
-        }
-
-        //ranges
-        double a = pow(-10,7);
-        double b = pow(10,7);
-
-        //test of range 10 - 20
-        for (int j = 0; j < size; j++)
-        {
-            //control input
-            if ((numbers[j] > a) && (numbers[j] < b))
-            {
-                //10 - 20
-                if ((numbers[j] >= 10) && (numbers[j] <= 20))
-                    countIn += 1;
-                else
-                    countOut += 1;
-            }
-        }
-
-        //output
-        printf("%d in\n", countIn);
-        printf("%d out\n", countOut);
+        scanf("%d", &numbers[i]);
     }
+
+    //ranges
+    double a = pow(-10,7);
+    double b = pow(10,7);
+
+    //test of range 10 - 20
+    for (int j = 0; j < size; j++)
+    {
+        //control input
+        if ((numbers[j] > a) && (numbers[j] < b))
+        {
+            //10 - 20
+            if ((numbers[j] >= 10) && (numbers[j] <= 20))
+                countIn += 1;
+            else
+                countOut += 1;
+        }
+    }
+
+    //output
+    printf("%d in\n", countIn);
+    printf("%d out\n", countOut);
 
     return 0;
 }
