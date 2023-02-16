3f8a04b52dd4ace8af60bc8cba2201a435aa7d40
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-02-15 20:18:40 -0400

    codigo finalizado e testado de sumConsecutiveOddNumber2.c

--- beecrowd/SumConsecutiveOddNumbersII/sumConsecutiveOddNumber2.c
+++ beecrowd/SumConsecutiveOddNumbersII/sumConsecutiveOddNumber2.c
@@ -7,13 +7,14 @@ int main(void)
     int x = 0;
     int y = 0; 
     int difference = 0;
-    int numberControl = 1;
-
+    int numberControl = 0;
+    
     //input
     scanf("%d", &n);
 
     //array of sum odd numbers
     int sumOdd[n];
+    int arrayOdd[n];
 
     //input diferences numbers
     for (int i = 0; i < n; i++)
@@ -23,21 +24,41 @@ int main(void)
 
         //solved difference, without negative number
         if ((x - y) < 0)
+        {
             difference = y - x; 
+            numberControl = x += 1;
+        }
         else 
+        {
             difference = x - y; 
-
+            numberControl = y += 1;
+        }
+            
         //correcting difference
-        if (difference != 0)
+        if (difference > 0)
             difference--;
         
+        //variable restart
+        int oddNumberCount = 0;
+
         //calc odd
         for (int j = 0; j < difference; j++)
         {
-            
-        }
-        
+            if ((numberControl % 2) != 0)
+                oddNumberCount += numberControl;
 
+            numberControl++;
+        }
+
+        //insert in array of odd
+        arrayOdd[i] = oddNumberCount;
+    }
+
+    //output
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", arrayOdd[i]);
     }
     
+    return 0;
 }
