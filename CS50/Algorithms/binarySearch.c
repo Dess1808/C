bb1b6b41b863cac4cbc0f13b12b2ccdc187c25b2
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-02-16 13:58:18 -0400

    terminar codigo

---
+++ CS50/Algorithms/binarySearch.c
@@ -0,0 +1,22 @@
+/*
+    para o Binary search funcionar, e preciso alinhar a sequencia de numeros primeiro!
+*/
+
+#include<stdio.h>
+#include<cs50.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int target = 19;
+    int start = 0;
+    int end = 0;
+    int middle = 0;
+
+    int arrayElements[] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 23, 25, 30};
+    int sizeArray = sizeof(arrayElements);
+
+    start  = sizeArray - sizeArray;
+    end = sizeArray;
+    middle = sizeArray / 2;
+}
