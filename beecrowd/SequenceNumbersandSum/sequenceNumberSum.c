a76e0a11606e46d9eea6bf7f36bf6e6a5ca24ab2
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-02-15 20:50:09 -0400

    versao funcionando, porem, com entrada e logo depois um saida com a soma - sequenceNumberSum.c

---
+++ beecrowd/SequenceNumbersandSum/sequenceNumberSum.c
@@ -0,0 +1,59 @@
+/*
+    repetir enquando uma das entradas dor < || == 0
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    //control variable
+    int controlLoop = 1;
+    int m = 0;
+    int n = 0;
+    int difference = 0;
+    int numberControl = 0;
+    
+
+    while(controlLoop)
+    {
+        //input
+        scanf("%d %d", &m, &n);
+
+       //check flux main
+       if ((m > 0) || (n > 0))
+       {
+            //check difference count
+            if ((m - n) < 0)
+            {
+                difference = (n - m) + 1;
+                numberControl = m;
+            }
+            else
+            {
+                difference = (m - n) + 1;
+                numberControl = n;
+            } 
+
+            //restart variable control of sum
+            int sumNumber = 0;
+
+            //output sequence number
+            for (int i = 0; i < difference; i++)
+            {
+                printf("%d ", numberControl);
+
+                sumNumber += numberControl;
+                numberControl++;    
+            }
+
+            //output sum
+            printf("Sum = %d\n", sumNumber);
+       }
+       else 
+       {    
+            controlLoop = 0;
+       }
+    }
+
+    return 0;
+}
