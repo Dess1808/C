79b9b9c69fcf859eb7a4851b91cfa45108d6016f
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-02-27 11:00:05 -0400

    continuar processo de pesquisa de problema

--- CS50/Algorithms/binarySearch.c
+++ CS50/Algorithms/binarySearch.c
@@ -2,7 +2,22 @@
     para o Binary search funcionar, e preciso alinhar a sequencia de numeros primeiro!
 
     worst-case-scenario = O(log N)
-    best-case-scenario = (n)  
+    best-case-scenario = (n)
+
+
+    logic:
+    If no doors left
+        Return false
+    If 50 is behind doors[middle]
+        Return true
+    Else if 50 < doors[middle]
+        Search doors[0] through doors[middle - 1]
+    Else if 50 > doors[middle]
+        Search doors[middle + 1] through doors[n - 1]
+
+    como calcular automaticamente o tamanho do array em c:
+    https://www.scaler.com/topics/length-of-an-array-in-c/
+
 */
 
 #include<stdio.h>
@@ -16,7 +31,11 @@ int main(void)
     int end = 0;
     int middle = 0;
 
-    int arrayElements[] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 23, 25, 30};
+    int doors[] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 23, 25, 30};
     
-    int sizeArray = sizeof(arrayElements);
+    int nSize = sizeof(doors);
+
+    printf("size doors is %d\n", nSize);
+
+    return 0;
 }
