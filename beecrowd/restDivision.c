821e68e36169168dd08d017037479a9d0dc9f638
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-03-06 13:55:05 -0400

    feito somento o tratamento das entradas, continuar para calculos de between

---
+++ beecrowd/restDivision.c
@@ -0,0 +1,35 @@
+/*
+    resto de divisao por 5 == 2 or 3
+
+    estabelecer range ENTRE numeros inseridos
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    int x = 0;
+    int y = 0;
+
+    //x
+    do
+    {
+        scanf("%d", &x);
+
+        if (x >= 0)
+            break;
+    } while (1);
+
+    
+    //y
+    do
+    {   
+        scanf("%d", &x);
+
+        if (x >= 0)
+            break;
+    } while (1);
+
+    //calc between
+    
+}
