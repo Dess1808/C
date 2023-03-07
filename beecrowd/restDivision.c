4383a19bf720c4296b98dd6149c40226226dfcad
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-03-07 12:39:55 -0400

    finalizado codigo, funcionando - restDivision.c

--- beecrowd/restDivision.c
+++ beecrowd/restDivision.c
@@ -6,10 +6,14 @@
 
 #include<stdio.h>
 
+void Show(int i);
+
 int main(void)
 {
     int x = 0;
     int y = 0;
+    int sizeBetween = 0;
+    int less = 0;
 
     //x
     do
@@ -20,16 +24,49 @@ int main(void)
             break;
     } while (1);
 
-    
+
     //y
     do
-    {   
-        scanf("%d", &x);
+    {
+        scanf("%d", &y);
 
-        if (x >= 0)
+        if (y >= 0)
             break;
     } while (1);
 
     //calc between
-    
+    if (x > y)
+    {
+        less = y;
+        sizeBetween = (x - y) - 1;
+    }
+    else if (x < y)
+    {
+        less = x;
+        sizeBetween = (y - x) - 1;
+    }
+    else
+    {
+        sizeBetween = x - y;
+    }
+
+    //count + calc of rest
+    for (int i = 0; i < sizeBetween; i++)
+    {
+        less++;
+
+        if ((less % 5 == 2) || (less % 5 == 3)) //verificar se esta certo
+        {
+            Show(less);
+        }
+        else
+            continue; //pula para o proximo
+    }
+
+    return 0;
+}
+
+void Show(int less)
+{
+    printf("%d\n", less);
 }
