9359121d10e23e6cdef0e1dc6d81f5500c71751d
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-03-24 13:56:02 -0400

    finalizado e funcionando - sumConsecutiveOddNUmbersII

---
+++ beecrowd/sumConsecutiveOddNumbersIII.c
@@ -0,0 +1,82 @@
+/*
+    condicoes:
+    se y for par, somar para pegar o proximo impar
+    para y testes, utilizar somente impares
+    para y teste soma-los
+*/
+
+#include<stdio.h>
+
+void Show(int s);
+
+int main(void)
+{
+    //control variable
+    int n = 0;
+    int countOdd = 1;
+    int sumOddX = 0;
+    int x, y = 0;
+    int control  = 0;
+
+    //input
+    scanf("%d", &n);    
+
+    for (int i = 0; i < n; i++)
+    {
+        //x, y
+        scanf("%d %d", &x, &y);
+
+        //condicao para inicio, par ou impar
+        if ((x % 2) != 0)
+        {
+            //impar
+            printf("%d\n", SumOddNumbers(x, y));
+        }
+        else 
+        {
+            //par
+
+            //atualizando para o proximo numero impar
+            x++;
+            printf("%d\n", SumOddNumbers(x, y));
+        }
+    }
+}
+
+void Show(int s)
+{
+    printf("%d\n", s);
+}
+
+int SumOddNumbers(int x, int y)
+{
+    //control variable
+
+    //reset variable
+    countOdd = x;
+
+    while(1)
+    {
+        //validation numbers
+        if ((countOdd % 2) != 2)
+        {
+            //sum all odds
+            sumOddX += countOdd;
+            
+            //update counters
+            countOdd++;
+            control++;
+        }
+        else
+        {
+            countOdd++;
+            continue;
+        } 
+            
+        //base case
+        if (control == y)
+        {
+            Show(sumOddX);
+            break;
+        }
+}
