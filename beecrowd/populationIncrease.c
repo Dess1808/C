dfa951e3c44d6a897ecfcf7483760ea93f14cdc6
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-03-29 10:52:20 -0400

    calculo de taxa de crescimento esta incorreto, continuar verificando

--- beecrowd/populationIncrease.c
+++ beecrowd/populationIncrease.c
@@ -1,9 +1,44 @@
 /*
   aplicando porcentagem
-  resulPA = ((PA * G2) /100) + PA  
-  resulPB = ((PB * G1) /100) + PB  
+  resulPA = ((PA * G1) /100) + PA  
+  resulPB = ((PB * G2) /100) + PB  
 
 output:
     resultPA - resultPB
 */
 
+#include<stdio.h>
+
+#define CENTURY 100
+
+int main(void)
+{
+  //controle variable
+  int t = 0;
+  int pa, pb, popIncrese = 0;
+  float g1, g2, resultPa, resultPb = 0.0;
+
+  //input count
+  scanf("%d", &t);
+
+  for (int i = 0; i < t; i++)
+  {
+    //input 
+    scanf("%d %d %f %f", &pa, &pb, &g1, &g2);
+
+    //calc pa and pb
+    resultPa = ((pa * g1) / 100) + pa;
+    resultPb = ((pb * g2) / 100) + pb;
+
+    //population increse result
+    popIncrese = (int) resultPb - (int) resultPa;
+
+    //selection output
+    if (popIncrese >= CENTURY)
+      printf("Mais de 1 seculo.\n");
+    else 
+      printf("%d anos.\n", popIncrese);
+  }
+
+  return 0;
+}
