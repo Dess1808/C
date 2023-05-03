3900d45bd52e412e723e0f9f4bc81d1efc55c4a9
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-03 18:32:26 -0400

    logica finalizada e funcionando, mas nao pronto, verificar EOF

--- beecrowd/array123.c
+++ beecrowd/array123.c
@@ -1,5 +1,23 @@
 /*
     preencher todo o array de 3 e depois cruzar 1 e 2
+
+    HIPOTESE 1:
+    utilizar matematica
+
+    - a diferenca entre 1 e 2 e sempre será as posicoes -2 na interação +1 (proxima)
+    interação.
+
+    - pego o valor de n e divido por 2 arredondando para + (tenho o index da metado do vetor)
+
+    - primeira posicoes fixas para toda matris 0,0 -> e 1, n - 1 -> 2
+
+    HIPOTESE 2:
+    Pura manipulação de index com condição
+
+    - sempre para o numero 1 os index sao iguais
+
+    - para o numero dois, e index da row com o index da column "n - 1", e continuar subtraindo a cada repeticao do for row, ate n ser 0 
+    assim, formando um x de 1 e 2 para qualquer tamanho de array bidimensional.
 */
 
 #include<stdio.h>
@@ -9,23 +27,80 @@ void Fill(int n, int array123[n][n]);
 
 const int ONE = 1;
 const int TWO = 2;
+const int DEFAULTVALUE = 3;
 
 int main(void)
 {
+    //variable control
     int n = 0;
+    int sizeAux = 0;
 
-    //input
-    scanf("%d", &n);
-
-    //matriz create
-    int array123[n][n];
-
-    //preenchendo com tres
-    Fill(n, array123); 
-
-    //make to X
-    for (int i = 0; i < n; i++) //
+    do
     {
-        
+        //reset variable
+        sizeAux = 0;
+
+        //input
+        scanf("%d", &n);
+
+        //array bid create
+        int array123[n][n];
+
+        //filling with the number three
+        Fill(n, array123); 
+
+        //sizeAux for number TWO, start last position
+        sizeAux = n - 1;
+
+        //make to X
+        for (int i = 0, size = n; i < size; i++) //row
+        {
+            for (int j = 0, size = n; j < size; j++)//column
+            {
+                //condition for one
+                if (i == j)
+                    array123[i][j] = ONE;
+
+                //condition for two
+                if (j == sizeAux)
+                    array123[i][j] = TWO;
+            }
+
+            //update variable
+            sizeAux--;
+
+        }//end for row
+
+        //output
+        Show(n, array123);
+
+    } while (n != EOF); //eof - end file, verificar esse end file 
+    
+
+    return 0;
+}
+
+
+void Fill(int n, int array123[n][n])
+{
+    for (int i = 0; i < n; i++)//row
+    {
+        for (int j = 0; j < n; j++) //column
+        {
+            array123[i][j] = DEFAULTVALUE;
+        }
+    }
+}
+
+void Show(int n, int array123[n][n])
+{
+    for (int i = 0; i < n; i++)//row
+    {
+        for (int j = 0; j < n; j++) //column
+        {
+            printf("%d", array123[i][j]);
+        }
+
+        putchar('\n');
     }
 }
