3c55c121c67bf9d489234a25227e9d9f1b4fc036
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-05 11:32:11 -0400

    finalizado e funcionando - buildingHouses

--- beecrowd/BuildingHouses.c
+++ beecrowd/BuildingHouses.c
@@ -6,25 +6,36 @@
     - metros quadrados dos dois valores inseridos
 
     regra de 3
-    metrosQuadrados --- x%
-    x ----------------- 100%
+    80 ---------------- 20%
+    x  ---------------- 100%
+
+    //formula principal
+    ladoFinal = sqrt(metrosQuadrado * 100 / variavelPorcentagem)
 
     - valores resultante, tirar raiz quadrada do mesmo
 */
 
 #include<stdio.h>
+#include<math.h>
 
 int main(void)
 {
-    int a, b, c = 0;
+    int ladoA, ladoB, porcentagem = 0;
 
-    do
+    while(1)
     {
         //input
-        scanf("%d%d%d", &a, &b, &c);
+        scanf("%d", &ladoA);
 
+        //end loop in first input
+        if (ladoA == 0)
+            break;
 
-    } while ((a != 0) || (b != 0) || (c != 0));
+        scanf("%d", &ladoB);
+        scanf("%d", &porcentagem);
+
+        printf("%d\n", (int)sqrt((ladoA * ladoB) * 100 / porcentagem));
+    }
     
     return 0;
 }
