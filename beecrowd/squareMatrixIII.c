acb15366ed1e84e85d53707c20089c6cc964bda8
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-05 12:40:57 -0400

    logica contruida, falta apenas descobrir a quantidade de digitos dos valores

---
+++ beecrowd/squareMatrixIII.c
@@ -0,0 +1,73 @@
+/*
+     - cada valor da matrix sera ele mesmo multiplicado por 2
+
+     - apartir da segunda linha o primeiro valor do index 0 sera o segunda valor da linha anterior
+*/
+
+#include<stdio.h>
+
+void Show(int n, int matrix[n][n]);
+
+int main(void)
+{
+    //control variable
+    int n = 0; 
+    int value = 0;
+    int valueAux = 0;
+    int tabulation = 0;
+
+    while(1)
+    {
+        //reset variable
+        value = 1;
+        valueAux = 0;
+        tabulation = 0;
+
+        //input
+        scanf("%d", &n);
+
+        //create array bidmensional
+        int matrix[n][n];
+
+        //end loop 
+        if (n == 0)
+            break;
+
+        //matrix double
+        for (int i = 0, size = n; i < size; i++)//row
+        {
+            for (int j = 0, size = n; j < size; j++) //column
+            {
+                matrix[i][j] = value;
+
+                value *= 2;
+
+                //second value
+                if (j == 1)
+                    valueAux = value;
+            }
+
+            //update value with second value
+            value = valueAux;
+        }
+
+        //for tabulation
+        
+
+        //output
+        Show(n, matrix);
+    }
+
+    return 0;
+}
+
+void Show(int n, int matrix[n][n])
+{
+    for (int i = 0, size = n; i < size; i++)//row
+        {
+            for (int j = 0, size = n; j < size; j++) //column
+            {   
+                printf("%*d", matrix[i][j]);
+            }
+        }
+}
