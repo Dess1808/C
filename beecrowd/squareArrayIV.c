c6abd416965d42e41e48ba0ee298c25f73e91d67
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-10 18:00:49 -0400

    feito a logica em comentarios, falta produzir o codigo

---
+++ beecrowd/squareArrayIV.c
@@ -0,0 +1,12 @@
+/*
+    primeiro: preencher inteira com 0
+
+    segundo: preencher uma matriz mais interna com 1, o tamanho dessa matriz sera
+    size - (size / 3) * 2 
+
+    terceiro: adicionar no centro da matriz o valor 4
+    
+    quarto: prencher as diagonais com 3 e 2, a quantidade de repetição sera size / 3
+    tanto no topo como no fim da matriz, de cima para baixo, a distancias entre elas diminue 
+    a cada repetição
+*/
