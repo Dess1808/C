05975c6f397b8cb5a468fb733c7c0c07d20db252
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-11 18:41:06 -0400

    verificar uma logica para diagonais, ta meio estranho

--- beecrowd/squareArrayIV.c
+++ beecrowd/squareArrayIV.c
@@ -5,7 +5,7 @@
      size - (size / 3) * 2 
 
     terceiro: adicionar no centro da matriz o valor 4
-    
+
     quarto: prencher as diagonais com 3 e 2, a quantidade de repetição sera size / 3
     tanto no topo como no fim da matriz, de cima para baixo, a distancias entre elas diminue 
     a cada repetição
@@ -27,8 +27,8 @@ int main(void)
     int n = 0;
     int nInside = 0;
     int startInside = 0;
-    int controlSizeUpColumn = 0;
-    int sizeUpColumn = 0;
+    int controlSizeUpRightColumn = 0;
+    int sizeUpRightColumn = 0;
 
     //end file
     while(scanf("%d", &n) != EOF)
@@ -51,22 +51,13 @@ int main(void)
         //number four center array
         squareArray[ceil(n/2)][ceil(n/2)] = FOUR;
 
-        //diagonais 
+        //diagonals 
 
         //up right
-        for (int i = 0, sizeUpRow = n / 3; i < sizeUpRow; i++) //row
-        {
-            for (int j = 0; i < sizeUpColumn; j++)//column
-            {
-                squareArray[i][j] = TWO;
-            }
-
-            //update
-            controlSizeUpColumn++;
-            sizeUpColumn = controlSizeUpColumn;
-        }
+        
 
         //up left
+        
         
 
     }
