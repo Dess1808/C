a9dcaf96575b778461ab75b76d532893732409f1
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-12 19:05:57 -0400

    debugar loop bazinga

--- beecrowd/bazinga.c
+++ beecrowd/bazinga.c
@@ -6,49 +6,96 @@
     tesoura - papel = tesoura vence! 4 (tesourapapel - tesoura)
     tesoura - lagarto = tesoura vence! 8 (tesouralagarto - tesoura)
 
-    papel - rocha = papel vence! -2
-    papel - Spock = papel vence! -3
+    papel - rocha = papel vence! -2 (papelrocha)
+    papel - Spock = papel vence! -3 (papelSpock)
     
-    rocha - lagarto = rocha vence! 6
-    rocha - tesoura = rocha vence! -2
+    rocha - lagarto = rocha vence! 6 (rochalagarto)
+    rocha - tesoura = rocha vence! -2 (rochatesoura)
     
-    lagarto - Spock = lagarto vence! -7
-    lagarto - papel = lagarto vence! -4 
+    lagarto - Spock = lagarto vence! -7 (lagartospock)
+    lagarto - papel = lagarto vence! -4  (lagartopapel)
     
-    Spock - tesoura = Spock vence! -1
-    Spock - rocha = Spock vence! 1
+    Spock - tesoura = Spock vence! -1 (spocktesoura)
+    Spock - rocha = Spock vence! spockrocha
     
     palavras iguais = jogo empatado
 
     fazer teste para as duas combincoes possivel, diverente disso raj ganha
 
-    
+    outputs
+    bazinga! - sheldon ganha
+    Raj trapaceou! - sheldon perde
+    De novo! - jogo empatado
 */
 
 #include<stdio.h>
 #include<string.h>
 
+int const SIZEARRAY = 10;
+int const SIZEWORD = 20;
+
+void Show(char *s1, int r);
+
 int main(void)
 {
     //control variable center
-    char tesouraPapel[] = "tesourapapel";
-    char tesouraLagarto[] = "tesouralagarto";
-
-    char papel[] = "papel";
-    char lagarto[] = "lagarto";
-
+    char bazingaArray[SIZEARRAY][SIZEWORD] = {"tesourapapel", "tesouralagarto", "papelrocha", "papelSpock", "rochalagarto", "rochatesoura", "lagartospock", "lagartopapel", "spocktesoura", "spockrocha"}; 
     char sheldon[10];
     char raj[10];
+    char *game;
 
-    //input
-    scanf("%s%s", sheldon, raj);
+    int t = 0;
+    int countCase = 1;
+    int rajTrapaceou = 1;
 
-    char *sheldonRaj = strcat(sheldon, raj);
+    //input t times
+    scanf("%d", &t);
 
-    if (((strcmp(sheldonRaj, tesouraPapel)) == 0) || ((strcmp(sheldonRaj, tesouraLagarto)) == 0))
-        printf("Bazinga!\n");
-    else
-        printf("Raj trapacou!\n"); 
+    do
+    {
+        //reset variable
+        rajTrapaceou = 1;
+
+        //input games option
+        scanf("%s%s", sheldon, raj);
+        
+        //if main DEBUGAR !!!
+        if (strcmp(sheldon, raj) != 0)//not iquals
+        {
+            //concat of sheldon and raj
+            game = strcat(sheldon, raj);
+
+            //loop for bazinga
+            for (int i = 0, size = SIZEARRAY; i < size; i++)
+            {
+                if (strcmp(game, bazingaArray[i]) == 0)
+                {
+                    Show("Bazinga!", countCase); 
+                    rajTrapaceou = 0;
+                    break;
+                }      
+            }
+
+            //condition for raj
+            if (rajTrapaceou)
+                Show("Raj Trapacou!", countCase);
+        } 
+        else 
+        {
+            Show("De novo!", countCase);
+        }
+
+        countCase++;
+               
+    } while (countCase <= t);
+    
+
+    
 
     return 0;
+}
+
+void Show(char *s1, int r)
+{
+    printf("Caso #%d: %s\n", r, s1);
 }
