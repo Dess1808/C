91595a08e323e271213d0a7b7a4451f84ab1e493
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-16 13:07:13 -0400

    finalizado e funcionando, feito com swtich - bazinga

--- beecrowd/bazinga.c
+++ beecrowd/bazinga.c
@@ -37,16 +37,16 @@
 #include<string.h>
 
 //defines of differences of chars, win possibilts Sheldon
-#define tesouraPapel 11;
-#define tesouraLagarto 24;
-#define papelPedra 8;
-#define papelSpock 15;
-#define pedraLagarto 5;
-#define pedraTesoura -19;
-#define lagartoSpock 2;
-#define lagartoPapel -13;
-#define spockTesoura -26;
-#define spockPedra -7;
+#define tesouraPapel 11
+#define tesouraLagarto 24
+#define papelPedra 8
+#define papelSpock 15
+#define pedraLagarto 5
+#define pedraTesoura -19
+#define lagartoSpock 2
+#define lagartoPapel -13
+#define spockTesoura -26
+#define spockPedra -7
 
 
 void Show(char *s1, int r);
@@ -67,26 +67,71 @@ int main(void)
     do
     {
         //reset variables
+        dif = 0;
         
-        
-
         //input 
         scanf("%s%s", sheldon, raj);
 
         //selection test
         if (strcmp(sheldon, raj) != 0)
         {
+            //difference
             dif = (sheldon[0] - raj[0]) + (sheldon[1] - raj[1]) + (sheldon[2] - raj[2]);
+
+            switch (dif)
+            {
+                case tesouraPapel:
+                    Show("Bazinga!", countCase);
+                    break;
+
+                case tesouraLagarto:
+                    Show("Bazinga!", countCase);
+                    break;
+                
+                case papelPedra:
+                    Show("Bazinga!", countCase);
+                    break;
+                
+                case papelSpock:
+                    Show("Bazinga!", countCase);
+                    break;
+
+                case pedraLagarto:
+                    Show("Bazinga!", countCase);
+                    break;
+                
+                case pedraTesoura:
+                    Show("Bazinga!", countCase);
+                    break;
+                
+                case lagartoPapel:
+                    Show("Bazinga!", countCase);
+                    break;
+
+                case lagartoSpock:
+                    Show("Bazinga!", countCase);
+                    break;
+                
+                case spockPedra:
+                    Show("Bazinga!", countCase);
+                    break;
+                
+                case spockTesoura:
+                    Show("Bazinga!", countCase);
+                    break;
+
+                default:
+                    Show("Raj trapaceou!", countCase);
+                    break;
+            }//end switch
         }
         else 
         {
             Show("De novo!", countCase);
         }
 
-        
-        
-
         countCase++;
+
     } while (countCase <= t);
 
     return 0;
