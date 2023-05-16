07f766d78d1a62140e64edd55e80849bb36f858d
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-16 11:54:03 -0400

    logica concentrada nas vitorias de sheldon, foi analisado todas as possibilidades de votoria do mesmo e sera utilizado para os testes

--- beecrowd/bazinga.c
+++ beecrowd/bazinga.c
@@ -1,23 +1,28 @@
 /*
     como faco para saber se Sheldon ganhou ou perdou, (sheldon sempre esquerda)
 
-    todas as combinacoes:
+    todas as combinacoes de vitoria:
 
-    tesoura - papel = tesoura vence! 4 (tesourapapel - tesoura)
-    tesoura - lagarto = tesoura vence! 8 (tesouralagarto - tesoura)
+    tesoura - papel = tesoura vence! 11, -11 (tesourapapel - tesoura)
+    tesoura - lagarto = tesoura vence! 24, -24 (tesouralagarto - tesoura)
 
-    papel - rocha = papel vence! -2 (papelrocha)
-    papel - Spock = papel vence! -3 (papelSpock)
+    papel - pedra = papel vence 8, -8 (papelrocha)
+    papel - Spock = papel vence! 15, -15 (papelSpock)
     
-    rocha - lagarto = rocha vence! 6 (rochalagarto)
-    rocha - tesoura = rocha vence! -2 (rochatesoura)
+    pedra - lagarto = rocha vence! 5, -5(rochalagarto)
+    pedra - tesoura = rocha vence!  -19, 19 (rochatesoura)
     
-    lagarto - Spock = lagarto vence! -7 (lagartospock)
-    lagarto - papel = lagarto vence! -4  (lagartopapel)
+    lagarto - Spock = lagarto vence! 2, -2 (lagartospock)
+    lagarto - papel = lagarto vence! -13, 13  (lagartopapel)
     
-    Spock - tesoura = Spock vence! -1 (spocktesoura)
-    Spock - rocha = Spock vence! spockrocha
+    Spock - tesoura = Spock vence! -26, 26 (spocktesoura)
+    Spock - pedra = Spock vence! -7, 7 spockrocha
     
+
+    identificar o jogo ganhador
+    verificar quem vez o jogo ganhador
+
+
     palavras iguais = jogo empatado
 
     fazer teste para as duas combincoes possivel, diverente disso raj ganha
@@ -31,6 +36,19 @@
 #include<stdio.h>
 #include<string.h>
 
+//defines of differences of chars, win possibilts Sheldon
+#define tesouraPapel 11;
+#define tesouraLagarto 24;
+#define papelPedra 8;
+#define papelSpock 15;
+#define pedraLagarto 5;
+#define pedraTesoura -19;
+#define lagartoSpock 2;
+#define lagartoPapel -13;
+#define spockTesoura -26;
+#define spockPedra -7;
+
+
 void Show(char *s1, int r);
 
 int main(void)
@@ -38,75 +56,37 @@ int main(void)
     //control variable center
     char sheldon[10];
     char raj[10];
-    char *game;
 
     int t = 0;
     int countCase = 1;
-    int casee = 1;
-    int rajCheated = 1;
+    int dif = 0;
 
     //input t times
     scanf("%d", &t);
 
     do
     {
-        //reset variable
-        rajCheated = 1;
-
-        //input games option
-        scanf("%s%s", sheldon, raj);
+        //reset variables
         
-        //if main DEBUGAR !!! verificar logica desses ifs
-        if (strcmp(sheldon, raj) != 0)//not iquals
+        
+
+        //input 
+        scanf("%s%s", sheldon, raj);
+
+        //selection test
+        if (strcmp(sheldon, raj) != 0)
         {
-            //concat of sheldon and raj
-            game = strcat(sheldon, raj);
-
-            //selection test
-            if ((strcmp(game, "tesourapapel") == 0) || (strcmp(game, "tesouralagarto") == 0))
-            {
-                Show("Bazinga!", casee);
-                rajCheated = 0;
-            }
-                
-
-            if ((strcmp(game, "papelpedra") == 0) || (strcmp(game, "papelSpock") == 0))
-            {
-                Show("Bazinga!", casee);
-                rajCheated = 0;
-            }
-                
-            
-            if ((strcmp(game, "pedralagarto") == 0) || (strcmp(game, "pedratesoura") == 0))
-            {
-                Show("Bazinga!", casee);
-                rajCheated = 0;
-            }
-
-            if ((strcmp(game, "lagartoSpock") == 0) || (strcmp(game, "lagartopapel") == 0))
-            {
-                Show("Bazinga!", casee);
-                rajCheated = 0;
-            }
-
-            if ((strcmp(game, "Spocktesoura") == 0) || (strcmp(game, "Spockpedra") == 0))
-            {
-                Show("Bazinga!", casee);
-                rajCheated = 0;
-            }
-
-
-            //control for Raj
-            if (rajCheated)
-                Show("Raj trapaceou!", casee);
-        } 
+            dif = (sheldon[0] - raj[0]) + (sheldon[1] - raj[1]) + (sheldon[2] - raj[2]);
+        }
         else 
         {
             Show("De novo!", countCase);
         }
 
+        
+        
+
         countCase++;
-               
     } while (countCase <= t);
 
     return 0;
