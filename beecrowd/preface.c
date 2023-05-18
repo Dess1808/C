55010195b82b8620319e4ab6f4ed9fc0bd93f322
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-18 10:55:52 -0400

    estudar a forma definitiva de utilizar a teoria de Euclides para divisao de dois numeros

--- beecrowd/preface.c
+++ beecrowd/preface.c
@@ -32,21 +32,36 @@ int Abs(int x);
 
 int main(void)
 {
-    int dividend, divider, quocient, rest, restAbs, dividendAux, hcf = 0;
+    int dividend, divider, quocient, rest, restAbs, dividerAux, hcf = 0;
 
     //input variabel
     scanf("%d%d", &dividend, &divider);
 
     //rest absoluti calc
-    //restAbs = rest + Abs(divider);
+    restAbs = dividend%divider;
 
     //quocient calc
-    dividendAux = dividend;
+    dividerAux = divider;
 
-    //verificar bug
+    //verificar
     while(1)
     {
-        
+        //calc quocient and rest 
+        // quocient = dividend/divider;
+        // rest = dividend%divider;
+
+        // //rest condition
+        // if ((rest >= 0) && (rest < Abs(divider)))
+        // {
+        //     //output
+        //     printf("%d %d\n", quocient, rest);
+        //     break;
+        // }
+        // else
+        // {
+        //     dividend = divider;
+        //     divider = rest;
+        // }
     }
     
     return 0;
