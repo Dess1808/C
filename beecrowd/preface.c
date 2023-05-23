fb2117594f4d52c528941d934e71767fd25ae501
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-05-23 13:47:46 -0400

    zerei, so voltar pro codigo quando tiver a logica do calculo funcionando

--- beecrowd/preface.c
+++ beecrowd/preface.c
@@ -26,43 +26,12 @@
 
 
 #include<stdio.h>
-#include<math.h>
+//#include<math.h>
 
 int Abs(int x);
 
 int main(void)
 {
-    int dividend, divider, quocient, rest, restAbs, dividerAux, hcf = 0;
-
-    //input variabel
-    scanf("%d%d", &dividend, &divider);
-
-    //rest absoluti calc
-    restAbs = dividend%divider;
-
-    //quocient calc
-    dividerAux = divider;
-
-    //verificar
-    while(1)
-    {
-        //calc quocient and rest 
-        // quocient = dividend/divider;
-        // rest = dividend%divider;
-
-        // //rest condition
-        // if ((rest >= 0) && (rest < Abs(divider)))
-        // {
-        //     //output
-        //     printf("%d %d\n", quocient, rest);
-        //     break;
-        // }
-        // else
-        // {
-        //     dividend = divider;
-        //     divider = rest;
-        // }
-    }
     
     return 0;
 }
