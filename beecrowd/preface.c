919c0a73d0c757ec9c87123dd22b63569bce20e6
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-08-08 10:41:59 -0400

    alterado para versao mais recente, com um blobo corrigido, e o demais checados

--- beecrowd/preface.c
+++ beecrowd/preface.c
@@ -1,12 +1,96 @@
+#include<stdio.h>
+#include<math.h>
+
+int Abs(int x);
+void Show(int q, int r);
+
+int main(void)
+{
+    //control varible
+    int dividendo, divisor, resto, quoeficiente = 0;
+
+    //input
+    scanf("%d%d", &dividendo, &divisor);
+
+    //ok
+    if ((dividendo < 0) && (divisor > 0)) //-a, b -> o quoeficente tera que ser arredondado pra cima e o resto absoluto subtraido pelo b absoluto
+    {
+        quoeficiente = ceil((dividendo / divisor) - 1);
+
+        resto = divisor - Abs(dividendo % divisor);
+
+        //output
+        Show(quoeficiente, resto);
+    }
+    //ok
+    else if ((dividendo > 0) && (divisor < 0)) //a, -b -> apenas subtrair o b absoluto pelo resto absoluto, ou apenas tirar o modulo padrao de dividendo e divisor
+    {
+        quoeficiente = dividendo / divisor;
+
+        resto = dividendo % divisor;
+
+        //output
+        Show(quoeficiente, resto);
+    }
+    //ok
+    else if ((dividendo < 0) && (divisor < 0)) //-a, -b -> apenas o quoeficiente sera arredondado para cima
+    {
+        //preciso arredondar para cima quando o dividendo for maior negativamente
+        if (dividendo > divisor)
+            quoeficiente = ceil((dividendo / divisor) + 1);
+        else
+            quoeficiente  = dividendo / divisor;
+
+        //condition se 0
+        if ((Abs(dividendo % divisor)) != 0)
+            resto = Abs(divisor) - Abs(dividendo % divisor);
+        else
+            resto = 0;
+
+        //output
+        Show(quoeficiente, resto);
+
+    }
+    else //a, b -> divisao normal (quoeficiente)
+    {
+        quoeficiente = dividendo / divisor;
+        resto = dividendo % divisor;
+
+        Show(quoeficiente, resto);
+    }
+
+    return 0;
+}
+
+int Abs(int x)
+{
+    if (x < 0)
+    {
+        return x *= -1;
+    }
+    else
+    {
+        return x;
+    }
+}
+
+void Show(int q, int r)
+{
+    printf("%d %d\n", q, r);
+}
+
+
+
+
 /*
-    
+
     hipotese 1
     obter o resto e quociente de uma divisao
 
     quociente - parte inteira da divisao
     resto - modulo
 
-    duas funcoes 
+    duas funcoes
     EuclidesDivision() - retornar um valor inteiro atraves da formula (divisor * quoci + rest)
     Abs() - retorna o valor absoluto de um valor
 
@@ -20,7 +104,7 @@
 
     https://www.cuemath.com/numbers/euclids-division-algorithm/
 
-    entao teria duas condições 
+    entao teria duas condições
 
     a = b * q + r && 0<= r < b
 
@@ -30,7 +114,7 @@
     https://www.youtube.com/watch?v=9onmRUpb2xs
 
     possibilidades:
-    
+
     a, b -> divisao normal (quoeficiente)
 
     -a, b -> o quoeficente tera que ser arredondado pra cima e o resto absoluto subtraido pelo b absoluto
@@ -39,70 +123,3 @@
 
     -a, -b -> apenas o quoeficiente sera arredondado para cima
 */
-
-#include<stdio.h>
-#include<math.h>
-
-int Abs(int x);
-void Show(int q, int r);
-
-int main(void)
-{   
-    //control varible
-    int dividendo, divisor, resto, quoeficiente = 0;
-
-    //input 
-    scanf("%d%d", &dividendo, &divisor);
-
-    if ((dividendo < 0) && (divisor > 0))
-    {
-        //arredonda quoeficiente e b e o resto absolutos sao subtraidos
-        quoeficiente = floor((dividendo/divisor) - 1) ;
-        resto = Abs(divisor) - Abs(dividendo % divisor); //verificar bug de modulo
-
-        //output 
-        Show(quoeficiente, resto);
-    }
-    else if ((dividendo > 0) && (divisor < 0))
-    {
-        //apenas subtrair o b absoluto pelo resto absoluto
-        quoeficiente = dividendo/divisor;
-        resto = Abs(divisor) - Abs(dividendo % divisor);
-
-        //output 
-        Show(quoeficiente, resto);
-    }
-    else if ((dividendo < 0) && (divisor < 0))
-    {
-        //apenas o quoeficente sera arredondado
-        quoeficiente = floor((dividendo/divisor) - 1);
-        resto = dividendo%divisor;
-
-        //output 
-        Show(quoeficiente, resto);
-
-    }
-    else
-    {
-        //divisao normal
-        quoeficiente = dividendo/divisor;
-        resto = dividendo%divisor;
-
-        Show(quoeficiente, resto);
-    }
-
-    return 0;
-}
-
-int Abs(int x)
-{
-    if (x < 0)
-        return x *= -1;
-    else
-        return x;
-}
-
-void Show(int q, int r)
-{
-    printf("%d %d\n", q, r);
-}
