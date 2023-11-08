dfd58ca5f0394df1c46fa3229259ceb81dbb2150
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2023-11-08 19:38:28 -0400

    iniciar codigo de ambos

---
+++ livro-liguagemC/capitulo7/transform.c
@@ -0,0 +1,9 @@
+/**
+ * Problema: Recebe uma string e coloca toda a primeira metade em minusculas e a segunda metade em MAIUSCULA
+ * 
+ * exemplo:
+ * transform("mafalda") -> "mafALDA"
+ * 
+ * 
+ * 
+*/
---
+++ livro-liguagemC/capitulo7/wordupf.c
@@ -0,0 +1,21 @@
+/**
+ * Problema:
+ * Recebe uma string, coloca a primeira letra de cada palavra em maiuscula e as restantes em minusculas. separacao
+ * entre palavras e feita por espacos em brancos
+ * 
+ * hipotese:
+ * deixar tudo em lower case para depois deixar a primeira letra em upper case, ainda sem ideias
+ * 
+ * hipotese 2
+ * depois de cada espaco em branco deixar o proximo caracter em upper case
+ * 
+ * exemplo
+ * //lower first
+ * for (int i = 0; i < nSize; i++)
+ * {
+ *      string[i] = tolower(string[i]);
+ * } 
+ * 
+ * //verificacao
+ * 
+*/
