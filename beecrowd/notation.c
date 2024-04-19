812f97ff8d7d2ea294a6e34d7fb1e782740ba99a
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-04-19 11:29:13 -0400

    logica construida com sucesso, porem imprecisao com numeros longs

--- beecrowd/notation.c
+++ beecrowd/notation.c
@@ -8,37 +8,107 @@
  *  <===============
  *          X
  *
- *  para números positivos (maiores que 10, dividor por 10 || menores que 1, multiplicar por 10)
+ *  para números positivos (maiores 000,000000000,que 10, dividor por 10 || menores que 1, multiplicar por 10)
  *          X
  *   ================>
  * 0 1 ------------- 10
  *   <===============
  *          /
+ *
+ * passos
+ * [] definir se o positivo ou negativo
+ * [] rodar um loop dividindo o numero ou multiplicando ate o mesmo ficar entre 1 e 10
+ *    como faria para testar a parte inteira
+*/
+
+
+/**
+ * A LOGICA ESTA CERTO, POREM ESTA ACONTECENDO IMPRECISAO COM NUMEROS LONGS!!!
 */
 
 #include<stdio.h>
-
-const int TEN = 10;
-const int ONE = 1;
+#include<math.h>
 
 int main(void)
 {
     //variable control
     long double number = 0.0;
+    signed long long int whole_part = 0;
+    int count = 0;
 
     //input
     scanf("%Lf", &number);
 
+    //primeira atribuicao
+    whole_part = number;
+
     //condition main
-    if (number < 0)
+    if (number < 0) //negativos
     {
-        //negativos
-        printf("%.4Lf\n", number);
+        //identification
+        if (whole_part > -1) //maior que -1 "multiplicar"
+        {
+            while(whole_part > -1)
+            {
+                whole_part = number * 10;
+                number = number * 10;
+                count++;
+            }
+
+            //output
+            printf("%.4LfE-%d\n", number, count);
+        }
+        else if (whole_part < -10) //manor que -10 "dividir"
+        {
+           while(whole_part < -10)
+            {
+                whole_part = number / 10;
+                number = number / 10;
+                count++;
+            }
+
+            //output
+            printf("%.4LfE+%d\n", number, count);
+        }
+        else //numero no range, output sem calculo, so formatacao
+        {
+            printf("%.4LfE+00\n", number);
+        }
     }
-    else
+    else if (number == 0)//Fix 0
     {
-        //positivos
-        printf("%.4Lf\n", number);
+        printf("%.4LfE+00\n", number);
+    }
+    else //positivos
+    {
+        if (whole_part < 1) //menor que 1 "multiplicar"
+        {
+            while(whole_part < 1)
+            {
+                whole_part = number * 10;
+                number = number * 10;
+                count++;
+            }
+
+            //output
+            printf("%.4LfE-%d\n", number, count);
+        }
+        else if (whole_part > 10)//maior que 10 "dividir"
+        {
+            while(whole_part > 10)
+            {
+                whole_part = number / 10;
+                number = number / 10;
+                count++;
+            }
+
+            //output
+            printf("+%.4LfE+%d\n", number, count);
+        }
+        else //numero no range, output sem calculo, so formatacao
+        {
+           printf("+%.4LfE+00\n", number);
+        }
     }
 
     return 0;
