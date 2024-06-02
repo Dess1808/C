c5fadcf63513be4745b4c9bbd5910a2e67c137a7
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-06-02 13:15:03 -0400

    base contruida, porem, varificar calculo de perimetro de um poligno

---
+++ beecrowd/polygons.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    //control variables
+    int length, side = 0;
+
+    //input
+    scanf("%d%d", &side, &length);
+
+    //verificar calculo de perimetro!!!
+    //output
+    printf("%d\n", side * length);
+
+    return 0;
+}
