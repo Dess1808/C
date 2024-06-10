2ee494891cd7fae5c58a51c2157712141bec6640
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-06-10 11:53:42 -0400

    retirado a estrutura de verificação de entradas

--- beecrowd/polygons.c
+++ beecrowd/polygons.c
@@ -5,9 +5,6 @@ int main(void)
     //control variables
     int length, side = 0;
 
-    //input
-    scanf("%d%d", &side, &length);
-
     //verificar calculo de perimetro!!!
     //output
     printf("%d\n", side * length);
