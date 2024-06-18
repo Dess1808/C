f6fe312ed64a8830ee3a58584c6c7b0e2c23dd39
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-06-18 09:58:00 -0400

    definidos alguns esbocos sobre o problema

---
+++ beecrowd/roman.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+}
+
+/*
+tabela de dezenas
+10 X
+20 XX
+30 XXX
+40 XL
+50 L
+60 LX
+70 LXX
+80 LXXX
+90 
+100
+200
+300
+400
+500
+600
+700
+800
+900
+1000
+*/
