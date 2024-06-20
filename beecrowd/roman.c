1d12d4da5213ed82fa12f09d11b7a5cd2404b0fe
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-06-20 10:44:43 -0400

    esboco atualizado - roman.c

--- beecrowd/roman.c
+++ beecrowd/roman.c
@@ -2,11 +2,25 @@
 
 int main(void)
 {
+    int number = 0;
 
+    //output
+    scanf("%d", &number);
+
+    //conditions, testar todas a possibilidade, similar ao algo de "troco"
 }
 
 /*
 tabela de dezenas
+1 I
+2 II
+3 III
+4 IV
+5 V
+6 VI
+7 VII
+8 VIII
+9 IX
 10 X
 20 XX
 30 XXX
@@ -15,15 +29,17 @@ tabela de dezenas
 60 LX
 70 LXX
 80 LXXX
-90 
-100
-200
-300
-400
-500
-600
-700
-800
-900
-1000
+90 XC
+100 C
+200 CC
+300 CCC
+400 CD
+500 D
+600 DC 
+700 DCC
+800 DCCC
+900 DM
+1000 M
+
+preciso separar em unidade, dezena, centena e unidade milhar
 */
